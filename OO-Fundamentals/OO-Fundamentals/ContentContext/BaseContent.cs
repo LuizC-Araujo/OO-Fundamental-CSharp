e5d37dbd06ced9139b4693c82dc0a0ac5e007a21
Luiz Araujo <arauldesign@gmail.com>
2022-06-12 17:11:50 -0300

    Creating BaseContent class

---
+++ OO-Fundamentals/OO-Fundamentals/ContentContext/BaseContent.cs
@@ -0,0 +1,14 @@
+﻿using OO_Fundamentals.NotificationContext;
+using System;
+
+namespace OO_Fundamentals.ContentContext
+{
+    public abstract class BaseContent : Notifiable
+    {
+        public BaseContent()
+        {
+            Id = Guid.NewGuid(); // SPOF - Single Point of Failure
+        }
+        public Guid Id { get; set; }
+    }
+}
