f51e78fe34b5d7758a8f7a11db5f36833a49a9ad
Luiz Araujo <arauldesign@gmail.com>
2022-06-12 17:47:17 -0300

    creating SuscriptionContext and User and Student class

---
+++ OO-Fundamentals/OO-Fundamentals/SubscriptionContext/Student.cs
@@ -0,0 +1,11 @@
+﻿using OO_Fundamentals.SharedContext;
+
+namespace OO_Fundamentals.SubscriptionContext
+{
+    public class Student : BaseContent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ OO-Fundamentals/OO-Fundamentals/SubscriptionContext/User.cs
@@ -0,0 +1,10 @@
+﻿using OO_Fundamentals.SharedContext;
+
+namespace OO_Fundamentals.SubscriptionContext
+{
+    public class User : BaseContent
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
