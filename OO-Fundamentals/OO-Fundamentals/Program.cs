324e9b358fc8abd89007a7ccebad2ac291e8fbc9
Luiz Araujo <arauldesign@gmail.com>
2022-06-12 16:39:50 -0300

    Working with Lists

--- OO-Fundamentals/OO-Fundamentals/Program.cs
+++ OO-Fundamentals/OO-Fundamentals/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using OO_Fundamentals.ContentContext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
 namespace OO_Fundamentals
 {
@@ -6,7 +10,50 @@ namespace OO_Fundamentals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var articles = new List<Article>();
+            articles.Add(new Article("Artigo sobre OOP", "orientacao-objeto"));
+            articles.Add(new Article("Artigo sobre C#", "csharp"));
+            articles.Add(new Article("Artigo sobre .NET", "dotnet"));
+
+            foreach(var article in articles)
+            {
+                Console.WriteLine($"id: {article.Id}");
+                Console.WriteLine($"Title: {article.Title}");
+                Console.WriteLine($"Url: {article.Url}");
+                Console.WriteLine("------------------------");
+            }
+
+            var courses = new List<Course>();
+            var courseOOP = new Course("Fundamentos OOP", "fundamentos-oop");
+            var courseCSharp = new Course("Fundamentos C#", "fundamentos-csharp");
+            var courseAspNet = new Course("Fundamentos ASP.NET", "fundamentos-aspnet");
+
+            courses.Add(courseOOP);
+            courses.Add(courseCSharp);
+            courses.Add(courseAspNet);
+
+
+            var careers = new List<Career>();   
+            var careerDotNet = new Career("Especialista .NET", "especialista-dotnet");
+            var careerItem2 = new CareerItem(2, "Learn OOP", "", null);
+            var careerItem = new CareerItem(1, "Start here", "", null);
+            var careerItem3 = new CareerItem(3, "Learn .NET", "", null);
+
+            careerDotNet.Items.Add(careerItem2);
+            careerDotNet.Items.Add(careerItem);
+            careerDotNet.Items.Add(careerItem3);
+            careers.Add(careerDotNet);
+
+            foreach (var career in careers)
+            {
+                Console.WriteLine(career.Title);
+                foreach (var item in career.Items.OrderBy(x => x.Order))
+                {
+                    Console.WriteLine($"{item.Order} - {item.Title}");
+                }
+            }
+
+            Thread.Sleep(5000);
         }
     }
 }
