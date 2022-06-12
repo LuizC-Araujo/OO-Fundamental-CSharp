aae5311eff26361b3d44c5036d579f03265722f3
Luiz Araujo <arauldesign@gmail.com>
2022-06-12 18:14:34 -0300

    editing Student class

--- OO-Fundamentals/OO-Fundamentals/SubscriptionContext/Student.cs
+++ OO-Fundamentals/OO-Fundamentals/SubscriptionContext/Student.cs
@@ -1,4 +1,5 @@
-﻿using OO_Fundamentals.SharedContext;
+﻿using OO_Fundamentals.NotificationContext;
+using OO_Fundamentals.SharedContext;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,10 +7,25 @@ namespace OO_Fundamentals.SubscriptionContext
 {
     public class Student : BaseContent
     {
+        public Student()
+        {
+            CreateSubscription = new List<Subscription>();
+        }
         public string Name { get; set; }
         public string Email { get; set; }
         public User User { get; set; }
-        public IList<Subscription> Subscriptions { get; set; }
-        public bool IsPremium => Subscriptions.Any(x => !x.IsInactive);
+        public IList<Subscription> CreateSubscription { get; set; }
+        public bool IsPremium => CreateSubscription.Any(x => !x.IsInactive);
+
+        public void CreateSubscription(Subscription subscription)
+        {
+            if (IsPremium)
+            {
+                AddNotification(new Notification("Premium", "O aluno já tem uma assinatura ativa"));
+                return;
+            }
+
+            CreateSubscription.Add(subscription);
+        }
     }
 }
