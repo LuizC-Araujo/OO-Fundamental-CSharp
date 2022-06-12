81153c1c2aa60cf205ad2db2f46fbc255620e714
Luiz Araujo <arauldesign@gmail.com>
2022-06-12 17:11:23 -0300

    Creating Notifiable class

---
+++ OO-Fundamentals/OO-Fundamentals/NotificationContext/Notifiable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace OO_Fundamentals.NotificationContext
+{
+    public abstract class Notifiable
+    {
+        public List<Notification> Notifications { get; set; }
+
+        public void Add(Notification notification)
+        {
+            Notifications.Add(notification);
+        }
+
+        public void AddRange(IEnumerable<Notification> notifications)
+        {
+            Notifications.AddRange(notifications);
+        }
+    }
+}
