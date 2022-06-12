7fb033b8ab3aec57a5522f766e690914222bae5e
Luiz Araujo <arauldesign@gmail.com>
2022-06-12 17:30:25 -0300

    Testing changes

--- OO-Fundamentals/OO-Fundamentals/Program.cs
+++ OO-Fundamentals/OO-Fundamentals/Program.cs
@@ -35,7 +35,7 @@ namespace OO_Fundamentals
 
             var careers = new List<Career>();   
             var careerDotNet = new Career("Especialista .NET", "especialista-dotnet");
-            var careerItem2 = new CareerItem(2, "Learn OOP", "", courseOOP);
+            var careerItem2 = new CareerItem(2, "Learn OOP", "", null);
             var careerItem = new CareerItem(1, "Start here", "", courseCSharp);
             var careerItem3 = new CareerItem(3, "Learn .NET", "", courseAspNet);
 
@@ -50,8 +50,13 @@ namespace OO_Fundamentals
                 foreach (var item in career.Items.OrderBy(x => x.Order))
                 {
                     Console.WriteLine($"{item.Order} - {item.Title}");
-                    Console.WriteLine(item.Course.Title);
-                    Console.WriteLine(item.Course.Level);
+                    Console.WriteLine(item.Course?.Title);
+                    Console.WriteLine(item.Course?.Level);
+
+                    foreach(var notification in item.Notifications)
+                    {
+                        Console.WriteLine($"{notification.Property} - {notification.Message}");
+                    }
                 }
             }
 
