f5891ea01b58876789207007705825d139684a8f
Luiz Araujo <arauldesign@gmail.com>
2022-06-12 13:16:19 -0300

    initial commit

---
+++ OO-Fundamentals/OO-Fundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OO_Fundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
