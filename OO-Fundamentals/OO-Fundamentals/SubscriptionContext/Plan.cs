a215b5f10a62a595b1738505d8a04031943c6ab1
Luiz Araujo <arauldesign@gmail.com>
2022-06-12 17:59:34 -0300

    creating Plan and Subscription class

---
+++ OO-Fundamentals/OO-Fundamentals/SubscriptionContext/Plan.cs
@@ -0,0 +1,10 @@
+﻿using OO_Fundamentals.SharedContext;
+
+namespace OO_Fundamentals.SubscriptionContext
+{
+    public class Plan : BaseContent
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ OO-Fundamentals/OO-Fundamentals/SubscriptionContext/Subscription.cs
@@ -0,0 +1,13 @@
+﻿using OO_Fundamentals.SharedContext;
+using System;
+
+namespace OO_Fundamentals.SubscriptionContext
+{
+    public class Subscription : BaseContent
+    {
+        public Plan Plan { get; set; }
+        public DateTime? EndDate { get; set; }
+        public bool IsInactive => EndDate <= DateTime.Now;
+    }
+
+}
